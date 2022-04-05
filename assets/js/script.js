6ec5c5ad38e8e105476567eb23c6b719bd98df5f
manuel <manuelguzmanvilches@gmail.com>
2022-04-05 11:51:42 -0400

    agregando funcionalidades jquery

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+
+    $('#enviar').click(function () {
+        alert("El correo fue enviado correctamente");
+    });
+
+    $('body').on('dblclick', 'h4', function () {
+        $(this).css("color", 'red');
+    });
+
+    $('.card-title').click(function () {
+        $('.card-text').toggle();
+    });
+
+
+
+
+});
+
+
+
